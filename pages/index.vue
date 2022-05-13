b272be326c3820034c4dddd75049945b579da4d3
Darren Bennett <daz@dazeb.uk>
2022-05-13 01:06:17 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,10 @@
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  head() {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+    };
+  },
+};
 </script>
